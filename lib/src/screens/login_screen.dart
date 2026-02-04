5467ad6ef2da3527ff1037f484e53e3f23579ee1
Anush Manuja <anushmanuja@gmail.com>
2026-02-04 11:29:07 +0530

    Removed unnecessary file

--- lib/src/screens/login_screen.dart
+++
@@ -1,2 +0,0 @@
-// Sign-in UI removed. Authentication is not used in this project.
-// If you need to re-add sign-in UI, implement it in this file.
--- lib/src/screens/register_screen.dart
+++
@@ -1,2 +0,0 @@
-// Registration UI removed. Authentication is not used in this project.
-// If you need to re-add registration, implement it in this file.
